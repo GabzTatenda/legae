1f9e4d4752d7201f2b8db0b4703e63d7f3671b66
GabzTatenda <31762572+GabzTatenda@users.noreply.github.com>
2018-08-01 01:38:53 +0200

    Add files via upload

---
+++ leagae/includes/data.php
@@ -0,0 +1,60 @@
+<?php
+// database connection config
+$dbHost = 'localhost';
+$dbUser = 'root';
+$dbPass = 'gabztee';
+$dbName = 'leagae';
+
+$conn = mysql_connect ($dbHost, $dbUser, $dbPass) or die ('MySQL connect failed. ' . mysql_error());
+mysql_select_db($dbName) or die('Cannot select database. ' . mysql_error());
+
+$SERVER_PATH = "http://127.0.0.1/LEAGAE/";
+
+function dbQuery($sql)
+{
+	$result = mysql_query($sql) or die(mysql_error());
+	
+	return $result;
+}
+
+function dbAffectedRows()
+{
+	global $conn;
+	
+	return mysql_affected_rows($conn);
+}
+
+function dbFetchArray($result, $resultType = MYSQL_NUM) {
+	return mysql_fetch_array($result, $resultType);
+}
+
+function dbFetchAssoc($result)
+{
+	return mysql_fetch_assoc($result);
+}
+
+function dbFetchRow($result) 
+{
+	return mysql_fetch_row($result);
+}
+
+function dbFreeResult($result)
+{
+	return mysql_free_result($result);
+}
+
+function dbNumRows($result)
+{
+	return mysql_num_rows($result);
+}
+
+function dbSelect($dbName)
+{
+	return mysql_select_db($dbName);
+}
+
+function dbInsertId()
+{
+	return mysql_insert_id();
+}
+?>
