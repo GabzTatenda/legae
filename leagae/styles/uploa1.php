669dd0461af79e9658ee614c8c1a829d403f6c58
GabzTatenda <31762572+GabzTatenda@users.noreply.github.com>
2018-08-01 01:17:24 +0200

    Add files via upload

---
+++ leagae/styles/uploa1.php
@@ -0,0 +1,48 @@
+<?php
+include 'data.php';
+if(isset($_POST['btn-upload']))
+{    
+	$regNo = $_POST['RegNo'];
+	$pass = $_POST['pass'];
+	$email = $_POST['mail'];
+	
+	 $file = rand(1000,100000)."-".$_FILES['file']['name'];
+	 $file_loc = $_FILES['file']['tmp_name'];
+	 $file_size = $_FILES['file']['size'];
+	 $file_type = $_FILES['file']['type'];
+	 $folder="images/students/";
+ 
+ // new file size in KB
+ $new_size = $file_size/1024;  
+ // new file size in KB
+ 
+ // make file name in lower case
+ $new_file_name = strtolower($file);
+ // make file name in lower case
+ 
+ $final_file=str_replace(' ','-',$new_file_name);
+ 	 
+
+ if(move_uploaded_file($file_loc,$folder.$final_file))
+ {
+ $sql=mysqli_query($conn, "INSERT INTO studentcr VALUES(null,'$regNo','$pass'
+						,'$email','$final_file','$file_type','$new_size')");	
+
+  ?>
+  <script>
+  alert('successfully uploaded');
+        window.location.href='stdntadmsn1.php?success';
+        </script>
+  <?php
+ }
+ else
+ {
+  ?>
+  <script>
+  alert('error while uploading file');
+        window.location.href='stdntadmsn1.php?fail';
+        </script>
+  <?php
+ }
+}
+?>
