ded872db13c76b467c747a0cea10024d35562c25
GabzTatenda <31762572+GabzTatenda@users.noreply.github.com>
2018-08-01 02:04:56 +0200

    Add files via upload

---
+++ leagae/studentprof/reghdr.php
@@ -0,0 +1,18 @@
+
+		     <div class="wrapper row2">
+        <div class="rounded">
+            <nav id="mainav" class="clear">
+                <!-- ################################################################################################ -->
+                <ul class="clear">
+                    <li class="active"><a href="../admn.php">HOME</a></li>
+					<li><a href="onlview.php">View students</a></li>
+					<li><a href="../pages/studentlist.php">Edit students</a></li>
+					<li><a href="../stdntcredentials.php">Add students</a></li>
+					<li><a href="../pages/stdntadmsn1.php">Notifications</a></li>
+					<li><a href="../login/login.php">LOG OUT</a></li>
+                </ul>
+                <!-- ################################################################################################ -->
+            </nav>
+        </div>
+    </div>
+	
